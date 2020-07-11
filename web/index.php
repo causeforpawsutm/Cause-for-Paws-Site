1d5f56ff385b976a41030d14dcc930a28c6f3b27
andi <andi.fan@gmail.com>
2020-07-11 17:34:42 -0400

    hi

--- web/index.php
+++ web/index.php
@@ -21,7 +21,7 @@
 
 			//button to go back to home
 			
-			if (isset($_REQUEST['test2btn'])) {
+			if (!empty($_REQUEST['test2btn'])) {
 				$_SESSION['state']='home';
 				$view="home.php";
 				break;
@@ -37,7 +37,7 @@
 			// the view we display by default
 			$views="home.php";
 			
-			if (isset($_REQUEST['testbtn'])) {
+			if (!empty($_REQUEST['testbtn'])) {
 				$_SESSION['state']='test';
 				$view="test.php";
 				break;
