0bbe0dfb0a2e277dbd25a0114379607d0a31346c
andi <andi.fan@gmail.com>
2020-07-12 21:10:12 -0400

    hi

--- web/views/home.php
+++ web/views/home.php
@@ -11,7 +11,7 @@
   <title>Cause for Paws</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="../stylesheets/scrolling-nav.css" rel="stylesheet">
