7b493033dc28528bbf21929d35f23ed121a29f14
andi <andi.fan@gmail.com>
2020-08-11 16:02:59 -0400

    hi

--- web/views/home.php
+++ web/views/home.php
@@ -32,7 +32,8 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Home</a>
+	  <a class="navbar-brand js-scroll-trigger" href="#page-top">Home</a> 
+	  <header class="text-white" id="bg-header" style="background-image:url('../stylesheets/heroIMG.jpg')">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
