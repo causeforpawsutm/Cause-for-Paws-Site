cb45c42983c55466ac18559f2ed7741c4509564d
andi <andi.fan@gmail.com>
2020-08-12 19:54:46 -0400

    hi

--- web/views/home.php
+++ web/views/home.php
@@ -71,7 +71,7 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2 class="text-center">About us</h2>
-          <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
         </div>
       </div>
     </div>
@@ -93,7 +93,7 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2 class="text-center">Resources</h2>
-          <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
           <ul>
             <li>Resource 1</li>
             <li>Resource 2</li>
@@ -104,7 +104,7 @@
     </div>
   </section>
 
-  <section id="contact">
+  <section id="contact" class="bg-light">
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mx-auto">
