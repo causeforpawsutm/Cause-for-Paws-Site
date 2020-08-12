dcae326d9870ef6ae468a09e9888ed9248b49df4
andi <andi.fan@gmail.com>
2020-08-12 15:27:18 -0400

    hi

--- web/views/home.php
+++ web/views/home.php
@@ -1,21 +1,5 @@
 
-<?php
 
-if($_POST["submit"]) {
-    $recipient="andi.fan@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -125,20 +109,7 @@ if($_POST["submit"]) {
         <div class="col-lg-8 mx-auto">
           <h2 class="text-center">Contact us</h2>
 		  <p class="lead"></p>
-		  <?=$thankYou ?>
-
-    <form method="post" action="contact.php">
-        <label>Name:</label>
-        <input name="sender">
-
-        <label>Email address:</label>
-        <input name="senderEmail">
-
-        <label>Message:</label>
-        <textarea rows="5" cols="20" name="message"></textarea>
-
-        <input type="submit" name="submit">
-	</form>
+		  
 	
         </div>
       </div>
