4e9920a7ff3aca536ceed1ce983a25eb17e9d1bb
Kim Le <kimthn.le@gmail.com>
2020-09-13 02:02:10 -0400

    Change Event calendar design #3

--- web/views/home.php
+++ web/views/home.php
@@ -138,38 +138,40 @@
                     <p class="lead">
                         We will be throwing multiple events with animals, holding virtual events for the Fall semester, and having bake sales which will include vegetarian and vegan-friendly options!
                     </p>
-                    <div class="container">
-		<div class="row row-striped">
-			<div class="col-2 text-right">
-				<h1 class="display-4"><span class="badge badge-secondary">23</span></h1>
-				<h2>OCT</h2>
-			</div>
-			<div class="col-10">
-				<h3 class="text-uppercase"><strong>Ice Cream Social</strong></h3>
-				<ul class="list-inline">
-				    <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Monday</li>
-					<li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
-					<li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Cafe</li>
-				</ul>
-				<p>Lorem ipsum dolsit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-			</div>
-		</div>
-		<div class="row row-striped">
-			<div class="col-2 text-right">
-				<h1 class="display-4"><span class="badge badge-secondary">27</span></h1>
-				<h2>OCT</h2>
-			</div>
-			<div class="col-10">
-				<h3 class="text-uppercase"><strong>Operations Meeting</strong></h3>
-				<ul class="list-inline">
-				    <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Friday</li>
-					<li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
-					<li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Room 4019</li>
-				</ul>
-				<p>Lorem ipsum dolsit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-			</div>
-		</div>
-	</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container card text-left">
+                    <div class="row row-striped">
+                        <div class="col-2 text-right">
+                            <h1 class="display-4"><span class="badge badge-secondary">23</span></h1>
+                            <h2>OCT</h2>
+                        </div>
+                        <div class="col-10">
+                            <h4 class="text-uppercase"><strong>Ice Cream Social</strong></h3>
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Monday</li>
+                                <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                                <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Cafe</li>
+                            </ul>
+                            <p>Lorem ipsum dolsit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                    <div class="row row-striped">
+                        <div class="col-2 text-right">
+                            <h1 class="display-4"><span class="badge badge-secondary">27</span></h1>
+                            <h2>OCT</h2>
+                        </div>
+                        <div class="col-10">
+                            <h4 class="text-uppercase"><strong>Operations Meeting</strong></h3>
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Friday</li>
+                                <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
+                                <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Room 4019</li>
+                            </ul>
+                            <p>Lorem ipsum dolsit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -189,7 +191,7 @@
                         promotion of animal well-being.</p>
                     <div class="row align-self-end mx-auto py-4"><a href="https://ontariospca.ca/" target="_blank"><button type="button" class="btn btn-main">Website</button></a></div>
                 </div>
-                <div class="col-md-5 m-3 p-0 mx-auto card">
+                <div class="col-md-5 m-3 p-3 mx-auto card">
                     <img class="mx-auto" width="70%" src="../images/mhs-logo.jpg">
                     <h5 class="card-title">Mississauga Humane Society</h5>
                     <p class="card-body px-2 mb-0">Mississauga Humane Society advocates and cares for companion animals who have been neglected, discarded, given up, and abandoned, who seek shelter, basic necessities, and a second chance at
